1c1525c8aa7a0e2ec71a42c15ebcf1ec79068751
Akshay Gandhi <a.gandhi@axelor.com>
2022-12-12 13:25:42 +0530

    Add studio module and break dependency with exception module

---
+++ src/main/java/com/axelor/apps/bpm/service/execution/WkfEmailService.java
@@ -0,0 +1,31 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2022 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.bpm.service.execution;
+
+import com.axelor.apps.bpm.db.WkfTaskConfig;
+import com.axelor.apps.utils.context.FullContext;
+import javax.mail.MessagingException;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+public interface WkfEmailService {
+
+  public void sendEmail(WkfTaskConfig wkfTaskConfig, DelegateExecution execution)
+      throws ClassNotFoundException, MessagingException;
+
+  public String createUrl(FullContext wkfContext, String formName);
+}
