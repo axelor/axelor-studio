514161df1cc83ccf43b7e0468f2b2a5462e32ad7
Akshay Gandhi <a.gandhi@axelor.com>
2022-12-28 11:45:49 +0530

    RM#51771 Change folder data-apps to apps

--- src/main/java/com/axelor/studio/app/web/StudioAppInitLoader.java
+++ src/main/java/com/axelor/studio/app/web/StudioAppInitLoader.java
@@ -1,3 +1,20 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2022 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.axelor.studio.app.web;
 
 import com.axelor.app.AppSettings;
@@ -40,7 +57,7 @@ public class StudioAppInitLoader implements MethodInterceptor {
 
   private static final Logger log = LoggerFactory.getLogger(StudioAppInitLoader.class);
 
-  private static final String DATA_DIR_NAME = "data-apps";
+  private static final String DATA_DIR_NAME = "apps";
 
   private static final String APP_IMAGE = "image";
   private static final String APP_MODULES = "modules";
