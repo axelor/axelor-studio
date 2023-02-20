5524fee8460a0621b516474eea705e06047ad6a6
Akshay Gandhi <a.gandhi@axelor.com>
2023-02-20 16:57:58 +0530

    RM#56729,60234,59634 Studio Editor : Updates and fix issues

---
+++ src/main/webapp/studio/custom-model/precache-manifest.8a3ebb97ab1b96e422f145a613f97329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de4d3395c45d55adc13a0a0cc927bac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47e7ed731309c7fce514",
+    "url": "./static/css/main.f22b89c1.chunk.css"
+  },
+  {
+    "revision": "2b9b9300b4512225f064",
+    "url": "./static/js/2.c4f24982.chunk.js"
+  },
+  {
+    "revision": "ec406ee4360237332a305ebb6cbba0fe",
+    "url": "./static/js/2.c4f24982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e7ed731309c7fce514",
+    "url": "./static/js/main.69e3ebde.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
