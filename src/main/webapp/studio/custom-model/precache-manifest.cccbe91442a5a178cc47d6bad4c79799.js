ce2b80eee44884efef28d69bbc888287ee70ee59
Akshay Gandhi <a.gandhi@axelor.com>
2023-03-28 19:06:43 +0530

    RM#62418 Studio : Fix theme not apply

--- src/main/webapp/studio/custom-model/precache-manifest.cccbe91442a5a178cc47d6bad4c79799.js
+++ src/main/webapp/studio/custom-model/precache-manifest.cccbe91442a5a178cc47d6bad4c79799.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cc36d5f392180e367c969b3ee52d110f",
+    "revision": "e52d7db8b44df4fb48e1f0e4f144a904",
     "url": "./index.html"
   },
   {
-    "revision": "6ae6daa802a4d59b5234",
-    "url": "./static/css/main.f22b89c1.chunk.css"
+    "revision": "c37fce48ace284ae1735",
+    "url": "./static/css/main.fa84b9ed.chunk.css"
   },
   {
     "revision": "2b9b9300b4512225f064",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.c4f24982.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6ae6daa802a4d59b5234",
-    "url": "./static/js/main.2bb87fec.chunk.js"
+    "revision": "c37fce48ace284ae1735",
+    "url": "./static/js/main.7fc3b59c.chunk.js"
   },
   {
     "revision": "194ab46e6f8f75ded56d",
