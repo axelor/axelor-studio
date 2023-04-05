6b7401469090b2c27e6cd5651aaaff08acc57feb
Akshay Gandhi <a.gandhi@axelor.com>
2023-04-05 11:16:35 +0530

    RM#62575 WsTokenHandler: Add rollback on transactional

--- src/main/java/com/axelor/web/WsTokenHandler.java
+++ src/main/java/com/axelor/web/WsTokenHandler.java
@@ -40,9 +40,8 @@ public class WsTokenHandler {
 
   @GET
   @Path("/token")
-  @Transactional
+  @Transactional(rollbackOn = {Exception.class})
   public Response read(@Context UriInfo uri) throws URISyntaxException {
-    // TODO: Fix error when GlobalAuditInterceptor enabled
 
     MultivaluedMap<String, String> paramMap = uri.getQueryParameters();
 
