65e8ab527c47b64458fc3193cfdef52222d637f5
Akshay Gandhi <a.gandhi@axelor.com>
2023-05-12 10:34:27 +0200

    Studio : Update menus generation and changes in BPM editor
    Refs: STUDIO-61819, STUDIO-62138, STUDIO-62129, STUDIO-62139, STUDIO-63201, STUDIO-63443, STUDIO-63442, STUDIO-64043, STUDIO-63945

--- src/main/java/com/axelor/studio/bpm/service/deployment/WkfNodeService.java
+++ src/main/java/com/axelor/studio/bpm/service/deployment/WkfNodeService.java
@@ -17,18 +17,29 @@
  */
 package com.axelor.studio.bpm.service.deployment;
 
+import com.axelor.auth.db.Role;
+import com.axelor.auth.db.repo.RoleRepository;
 import com.axelor.inject.Beans;
 import com.axelor.meta.db.MetaAttrs;
 import com.axelor.studio.bpm.service.WkfCommonService;
 import com.axelor.studio.db.WkfModel;
 import com.axelor.studio.db.WkfTaskConfig;
+import com.axelor.studio.db.WkfTaskMenu;
+import com.axelor.studio.db.WkfTaskMenuContext;
 import com.axelor.studio.db.repo.WkfTaskConfigRepository;
+import com.axelor.studio.db.repo.WkfTaskMenuContextRepository;
+import com.axelor.studio.db.repo.WkfTaskMenuRepository;
 import com.google.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.camunda.bpm.engine.impl.bpmn.parser.BpmnParser;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.instance.Activity;
 import org.camunda.bpm.model.bpmn.instance.CatchEvent;
@@ -37,7 +48,9 @@ import org.camunda.bpm.model.bpmn.instance.EndEvent;
 import org.camunda.bpm.model.bpmn.instance.ExtensionElements;
 import org.camunda.bpm.model.bpmn.instance.FlowNode;
 import org.camunda.bpm.model.bpmn.instance.Process;
+import org.camunda.bpm.model.xml.impl.ModelBuilderImpl;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
+import org.camunda.bpm.model.xml.type.ModelElementType;
 
 public class WkfNodeService {
 
@@ -45,7 +58,13 @@ public class WkfNodeService {
 
   @Inject protected WkfTaskConfigRepository wkfConfigRepository;
 
+  @Inject protected WkfTaskMenuRepository wkfTaskMenuRepo;
+
   @Inject protected WkfMenuService wkfMenuService;
+
+  @Inject protected RoleRepository roleRepo;
+
+  @Inject protected WkfTaskMenuContextRepository taskMenuContextRepo;
 
   public List<MetaAttrs> extractNodes(
       WkfModel wkfModel, BpmnModelInstance bpmInstance, Map<String, String> processMap) {
@@ -136,6 +155,8 @@ public class WkfNodeService {
 
     ExtensionElements extensionElements = activity.getExtensionElements();
     if (extensionElements != null) {
+      computeTaskMenu(extensionElements, config);
+
       for (ModelElementInstance modelElementInstance : extensionElements.getElements()) {
         metaAttrsList.addAll(
             metaAttrsService.createMetaAttrs(
@@ -146,18 +167,145 @@ public class WkfNodeService {
     return config;
   }
 
+  private void computeTaskMenu(ExtensionElements extensionElements, WkfTaskConfig config) {
+
+    Map<String, WkfTaskMenu> menuMap = new HashMap<>();
+    if (CollectionUtils.isNotEmpty(config.getWkfTaskMenuList())) {
+      for (WkfTaskMenu menu : config.getWkfTaskMenuList()) {
+        menuMap.put(menu.getMenuId(), menu);
+      }
+    }
+
+    ModelElementInstance menusElement =
+        extensionElements.getUniqueChildElementByNameNs(
+            BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS, "menus");
+
+    if (menusElement != null) {
+      ModelBuilderImpl builderImpl = new ModelBuilderImpl(null);
+      ModelElementType menuElementType =
+          builderImpl.defineGenericType("menu", BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS);
+
+      Collection<ModelElementInstance> menuElements =
+          menusElement.getChildElementsByType(menuElementType);
+
+      for (ModelElementInstance menu : menuElements) {
+        WkfTaskMenu wkfTaskMenu = updateTaskMenu(menu, menuMap, builderImpl);
+        config.addWkfTaskMenuListItem(wkfTaskMenu);
+      }
+    }
+
+    for (String menuId : menuMap.keySet()) {
+      wkfMenuService.removeMenu(menuMap.get(menuId));
+      config.removeWkfTaskMenuListItem(menuMap.get(menuId));
+    }
+  }
+
+  private WkfTaskMenu updateTaskMenu(
+      ModelElementInstance menu, Map<String, WkfTaskMenu> menuMap, ModelBuilderImpl builderImpl) {
+
+    WkfTaskMenu taskMenu;
+
+    String menuId = menu.getAttributeValueNs(BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS, "menuId");
+    String isUserMenu =
+        menu.getAttributeValueNs(BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS, "isUserMenu");
+
+    menuId =
+        StringUtils.isNotEmpty(menuId)
+                && StringUtils.isNotEmpty(isUserMenu)
+                && isUserMenu.equals("true")
+            ? WkfMenuService.USER_MENU_PREFIX + menuId
+            : WkfMenuService.MENU_PREFIX + menuId;
+
+    if (menuMap.containsKey(menuId)) {
+      taskMenu = menuMap.get(menuId);
+      menuMap.remove(menuId);
+    } else {
+      String menuName = menu.getAttributeValueNs(BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS, "menuName");
+      taskMenu = new WkfTaskMenu();
+      taskMenu.setMenuId(menuId);
+      taskMenu.setMenuName(menuName);
+      wkfTaskMenuRepo.save(taskMenu);
+    }
+
+    taskMenu =
+        (WkfTaskMenu)
+            Beans.get(WkfCommonService.class)
+                .addProperties(WkfPropertyMapper.MENU_PROPERTIES, taskMenu, menu);
+
+    Set<Role> roleSet = null;
+    String roles = menu.getAttributeValueNs(BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS, "roles");
+    if (!StringUtils.isEmpty(roles)) {
+      roleSet = new HashSet<>();
+      String[] roleArr = roles.split(",");
+      for (String roleName : roleArr) {
+        Role role = roleRepo.findByName(roleName);
+        if (role == null) {
+          continue;
+        }
+        roleSet.add(role);
+      }
+    }
+    taskMenu.setRoleSet(roleSet);
+
+    computeMenuContext(menu, taskMenu, builderImpl);
+
+    return taskMenu;
+  }
+
+  private void computeMenuContext(
+      ModelElementInstance menu, WkfTaskMenu taskMenu, ModelBuilderImpl builderImpl) {
+
+    Map<String, WkfTaskMenuContext> contextMap = new HashMap<>();
+    if (CollectionUtils.isNotEmpty(taskMenu.getWkfTaskMenuContextList())) {
+      for (WkfTaskMenuContext context : taskMenu.getWkfTaskMenuContextList()) {
+        contextMap.put(context.getKey(), context);
+      }
+    }
+
+    ModelElementType menuContextType =
+        builderImpl.defineGenericType("context", BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS);
+
+    Collection<ModelElementInstance> menuContexts = menu.getChildElementsByType(menuContextType);
+
+    if (menuContexts != null && menuContexts.size() > 0) {
+      taskMenu.clearWkfTaskMenuContextList();
+      for (ModelElementInstance context : menuContexts) {
+        WkfTaskMenuContext menuContext = updateMenuContext(context, contextMap);
+        taskMenu.addWkfTaskMenuContextListItem(menuContext);
+      }
+    }
+
+    for (String key : contextMap.keySet()) {
+      taskMenu.removeWkfTaskMenuContextListItem(contextMap.get(key));
+    }
+  }
+
+  private WkfTaskMenuContext updateMenuContext(
+      ModelElementInstance context, Map<String, WkfTaskMenuContext> contextMap) {
+    WkfTaskMenuContext menuContext = null;
+
+    String key = context.getAttributeValueNs(BpmnParser.CAMUNDA_BPMN_EXTENSIONS_NS, "key");
+    if (contextMap.containsKey(key)) {
+      menuContext = contextMap.get(key);
+      contextMap.remove(key);
+    } else {
+      menuContext = new WkfTaskMenuContext();
+      menuContext.setKey(key);
+      taskMenuContextRepo.save(menuContext);
+    }
+
+    String value = context.getTextContent();
+    menuContext.setValue(value);
+
+    return menuContext;
+  }
+
   private void updateMenus(WkfTaskConfig taskConfig, boolean remove) {
 
-    if (!remove && taskConfig.getNewMenu()) {
+    if (!remove) {
       wkfMenuService.createOrUpdateMenu(taskConfig);
     } else {
       wkfMenuService.removeMenu(taskConfig);
-    }
-
-    if (!remove && taskConfig.getUserNewMenu()) {
-      wkfMenuService.createOrUpdateUserMenu(taskConfig);
-    } else {
-      wkfMenuService.removeUserMenu(taskConfig);
     }
   }
 }
