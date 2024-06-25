9ca4aeb787cda0a41c693699fedebb45f90cebb9
Fahime Abdillah <f.abdillah@axelor.com>
2024-06-25 17:47:59 +0100

    fix empty line issue when exporting templating

--- src/main/java/com/axelor/studio/service/builder/StudioAppServiceImpl.java
+++ src/main/java/com/axelor/studio/service/builder/StudioAppServiceImpl.java
@@ -369,7 +369,7 @@ public class StudioAppServiceImpl implements StudioAppService {
         Files.delete(file.toPath());
       } else {
         try (Stream<String> stream = Files.lines(file.toPath())) {
-          if (stream.count() == 1) {
+          if (stream.count() == 1 || ( Files.lines(file.toPath()).count() == 2 && Files.lines(file.toPath()).skip(1).findFirst().orElse(null).isEmpty())) {
             Files.delete(file.toPath());
           }
         }
