32fcce2f692837bb04f27ef4b97080a930f4f223
Fatiha BEN BAICH <f.benbaich@axelor.com>
2024-10-08 10:08:41 +0200

    STUDIO-84425 : Batch processing takes much longer in 3.2.0

--- src/main/java/com/axelor/studio/bpm/service/execution/WkfInstanceServiceImpl.java
+++ src/main/java/com/axelor/studio/bpm/service/execution/WkfInstanceServiceImpl.java
@@ -903,8 +903,6 @@ public class WkfInstanceServiceImpl implements WkfInstanceService {
 
   @Transactional(rollbackOn = Exception.class)
   protected void removeRelatedFailedInstance(Model model, WkfProcess wkfProcess) {
-    JPA.em().refresh(model);
-    JPA.em().refresh(wkfProcess);
     List<WkfInstance> instances =
         wkfInstanceRepository
             .all()
@@ -926,11 +924,10 @@ public class WkfInstanceServiceImpl implements WkfInstanceService {
     }
   }
 
-  @Transactional(rollbackOn = Exception.class)
   protected void setWkfInstanceError(WkfInstance wkfInstance, boolean value, String error) {
+    JPA.em().refresh(wkfInstance);
     wkfInstance.setInstanceError(value);
     wkfInstance.setCurrentError(error);
-    wkfInstanceRepository.save(wkfInstance);
   }
 
   @Override
