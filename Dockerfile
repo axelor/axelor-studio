b01825a6ffc037c2c684d5b56b154a8c676d3475
Lawrence Deneiple <l.deneiple@axelor.com>
2024-10-30 11:28:05 +0100

    build: update docker image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM axelor/app-builder:6.1 AS builder
+FROM axelor/app-builder:7.1 AS builder
 
 ARG JAVA_OPTS="-Xmx4g"
 ARG APP_SOURCE="/app/open-platform-demo"
@@ -13,41 +13,34 @@ RUN unzip app.zip
 RUN mv ${MODULE_NAME} ${APP_SOURCE}/modules
 WORKDIR ${APP_SOURCE}
 
+ARG NEXUS_READER_USERNAME
+ARG NEXUS_READER_PASSWORD
+
 RUN chmod +x gradlew && \
-    ./gradlew --no-daemon build -xtest -xcheck;
+    ./gradlew --no-daemon -xtest -xcheck -xspotlessApply clean build -PaxelorMavenUsername=${NEXUS_READER_USERNAME} -PaxelorMavenPassword=${NEXUS_READER_PASSWORD}
 
 RUN mkdir -p ${APP_SOURCE}/webapps/ROOT && \
     unzip -q -o ${APP_SOURCE}/build/libs/*.war -d ${APP_SOURCE}/webapps/ROOT/
 
-
 # Image to run tomcat with axelor-app
-FROM eclipse-temurin:11-jre-alpine
+FROM tomcat:9.0-jre11-temurin
 
+ARG MODULE_NAME="axelor-studio"
+ARG PROJECT_VENDOR="Axelor"
+ARG CATALINA_MEMORY_ALLOC_PCT=90
 ARG BUILD_DATE
-ARG TOMCAT_VERSION=9.0.85
 
-ARG APP_LANGUAGE
-ARG APP_DEMO_DATA
-ARG APP_LOAD_APPS
-ARG DEV_MODE
-ARG ENABLE_QUARTZ
+ENV CATALINA_OPTS="${CATALINA_OPTS} -Daxelor.ScriptCacheSize=500"
+ENV CATALINA_OPTS="${CATALINA_OPTS} -Daxelor.ScriptCacheExpireTime=10"
+ENV CATALINA_OPTS="${CATALINA_OPTS} -XX:InitialRAMPercentage=${CATALINA_MEMORY_ALLOC_PCT}"
+ENV CATALINA_OPTS="${CATALINA_OPTS} -XX:MaxRAMPercentage=${CATALINA_MEMORY_ALLOC_PCT}"
+ENV CATALINA_OPTS="${CATALINA_OPTS} ${AX_JAVA_OPTS}"
+ENV PROJECT_VERSION=${PROJECT_VERSION}
 
-ENV CATALINA_HOME="/usr/local/tomcat"
-
-ENV APP_LANGUAGE=${APP_LANGUAGE}
-ENV APP_DEMO_DATA=${APP_DEMO_DATA}
-ENV APP_LOAD_APPS=${APP_LOAD_APPS}
-ENV DEV_MODE=${DEV_MODE}
-ENV ENABLE_QUARTZ=${ENABLE_QUARTZ}
-
-# System Upgrade for security reasons + install tools needed by the entrypoint
-RUN apk upgrade && \
-    apk add --no-cache curl coreutils postgresql-client
-
-# Install Tomcat
-RUN export TOMCAT_MINOR_VERSION=$(echo ${TOMCAT_VERSION} | cut -d"." -f 1) && \
-    mkdir -p ${CATALINA_HOME} && \
-	  curl -L https://archive.apache.org/dist/tomcat/tomcat-${TOMCAT_MINOR_VERSION}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz | tar xvzf - --exclude="apache-tomcat*/webapps/*" --strip-components=1 --directory=${CATALINA_HOME}
+# Refresh repositories and install tools needed by the entrypoint
+RUN apt update && \
+    apt install -y curl coreutils postgresql-client && \
+    rm -rf /var/lib/apt/lists/*
 
 # Copy app
 COPY --from=builder /app/open-platform-demo/webapps ${CATALINA_HOME}/webapps
@@ -56,8 +49,8 @@ COPY --from=builder /app/open-platform-demo/webapps ${CATALINA_HOME}/webapps
 EXPOSE 8080
 
 # Copy entrypoint
-COPY docker-entrypoint.sh /usr/local/bin/
-RUN chmod +x /usr/local/bin/docker-entrypoint.sh
+COPY docker-entrypoint.sh /bin
+RUN chmod +x /bin/docker-entrypoint.sh
 
 # Label images
 LABEL \
@@ -65,8 +58,8 @@ LABEL \
 	org.label-schema.schema-version="1.0" \
 	org.label-schema.build-date="${BUILD_DATE}" \
 	org.label-schema.name="${MODULE_NAME}" \
-	org.label-schema.vendor="axelor"
+	org.label-schema.vendor="${PROJECT_VENDOR}"
 
 # Entrypoint
-ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
+ENTRYPOINT ["/bin/docker-entrypoint.sh"]
 CMD ["start"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/sh
 
 APP_USER="${APP_USER:-admin}"
 APP_PASS="${APP_PASS:-admin}"
@@ -9,10 +9,12 @@ PGPASSWORD="${PGPASSWORD:-axelor}"
 PGDATABASE="${PGDATABASE:-axelor}"
 
 APP_LANGUAGE="${APP_LANGUAGE:-en}"
-APP_DEMO_DATA="${APP_DEMO_DATA:-true}"
+APP_DEMO_DATA="${APP_DEMO_DATA:-false}"
 APP_LOAD_APPS="${APP_LOAD_APPS:-all}"
 DEV_MODE="${DEV_MODE:-false}"
 ENABLE_QUARTZ="${ENABLE_QUARTZ:-false}"
+CLEAN_DB="${CLEAN_DB:-false}"
+ENV_SHORT="${ENV_SHORT:-int}"
 
 APP_DATA_BASE_DIR="/data"
 APP_DATA_EXPORTS_DIR="${APP_DATA_EXPORTS_DIR:-$APP_DATA_BASE_DIR/exports}"
@@ -20,28 +22,19 @@ APP_DATA_ATTACHMENTS_DIR="${APP_DATA_ATTACHMENTS_DIR:-$APP_DATA_BASE_DIR/attachm
 
 mkdir -p ${APP_DATA_EXPORTS_DIR} ${APP_DATA_ATTACHMENTS_DIR}
 
-# Initialize tomcat
 init_tomcat() {
   echo "Configuring app:tomcat 🔧"
-
-  local TOMCAT_ENV_PATH=${CATALINA_HOME}/bin/setenv.sh
-  local TOMCAT_SERVER_PATH=${CATALINA_HOME}/conf/server.xml
-
-  # Add setenv.sh
-  cat > ${TOMCAT_ENV_PATH} <<EOF
-export CATALINA_PID="\$CATALINA_BASE/tomcat.pid"
-export JAVA_OPTS="\$JAVA_OPTS -Xms${JAVA_XMS:-2G} -Xmx${JAVA_XMX:-2G} -server ${AX_JAVA_OPTS}"
-EOF
+  TOMCAT_SERVER_PATH=${CATALINA_HOME}/conf/server.xml
 
   # Remove localhost_access_log
-  sed -i -E ':a;N;$!ba;s/<Valve className=\"org.apache.catalina.valves.AccessLogValve\"(.|\n)*\/>//' $TOMCAT_SERVER_PATH
+  sed -i -E -e ':a' -e 'N' -e '$!ba' -e 's|<Valve className="org.apache.catalina.valves.AccessLogValve"(.\|\n)*/>||' ${TOMCAT_SERVER_PATH}
 }
 
-# Wait for Postgres
 wait_for_postgres() {
-  local retries=5
+  retries=5
   until psql --command "SELECT 1" > /dev/null 2>&1 || [ ${retries} -eq 0 ]; do
-    echo "Waiting for postgres server, $((retries--)) remaining attempts..."
+    echo "Waiting for postgres server, ${retries} remaining attempts..."
+    retries=$((retries-1))
     sleep 3
   done
 
@@ -52,22 +45,20 @@ wait_for_postgres() {
   fi
 }
 
-# Initialize database
 init_postgres() {
-  if [[ -z "$(psql -t --command "SELECT extname FROM pg_extension WHERE extname = 'unaccent'";)" ]]; then
+  if [ -z "`psql -t --command "SELECT extname FROM pg_extension WHERE extname = 'unaccent'"`" ]; then
     echo "Configuring app:database 🔧"
     psql --command "CREATE EXTENSION IF NOT EXISTS unaccent"
   fi
 }
 
-# Update app properties
 update_properties() {
   echo "Configuring app:properties 🔧"
 
-  local APP_PROP_FILE_PATH="${CATALINA_HOME}/webapps/ROOT/WEB-INF/classes/axelor-config.properties"
-  local APP_MODE="prod"
-  local LOG_LEVEL="INFO"
-  if [[ "${DEV_MODE}" == "true" ]]; then
+  APP_PROP_FILE_PATH="${CATALINA_HOME}/webapps/ROOT/WEB-INF/classes/axelor-config.properties"
+  APP_MODE="prod"
+  LOG_LEVEL="INFO"
+  if [ "${DEV_MODE}" = "true" ]; then
     APP_MODE="dev"
     LOG_LEVEL="DEBUG"
   fi
@@ -82,24 +73,24 @@ update_properties() {
   findAndReplace "db.default.password" "${PGPASSWORD}" ${APP_PROP_FILE_PATH}
   findAndReplace "db.default.url" "jdbc:postgresql://${PGHOST}:5432/${PGDATABASE}" ${APP_PROP_FILE_PATH}
   findAndReplace "db.default.user" "${PGUSER}" ${APP_PROP_FILE_PATH}
-  findAndReplace "hibernate.hikari.maximumPoolSize" "10" ${APP_PROP_FILE_PATH}
+  findAndReplace "hibernate.hikari.maximumPoolSize" "20" ${APP_PROP_FILE_PATH}
   findAndReplace "hibernate.hikari.minimumIdle" "1" ${APP_PROP_FILE_PATH}
   findAndReplace "hibernate.search.default.directory_provider" "none" ${APP_PROP_FILE_PATH}
   findAndReplace "logging.level.com.axelor" "${LOG_LEVEL}" ${APP_PROP_FILE_PATH}
   findAndReplace "quartz.enable" "${ENABLE_QUARTZ}" ${APP_PROP_FILE_PATH}
   findAndReplace "temp.dir" "{java.io.tmpdir}" ${APP_PROP_FILE_PATH}
+  findAndReplace "encryption.password" "${ENCRYPTION_PASSWORD}" ${APP_PROP_FILE_PATH}
 }
 
-# Update a property
 findAndReplace() {
-  local PROP=$1
-  local VALUE=$2
-  local FILE=$3
+  PROP=$1
+  VALUE=$2
+  FILE=$3
 
   if grep -q "${PROP}" ${FILE}; then
-    sed -i "s/^${PROP}.*/${PROP} = $(echo "${VALUE}" | sed 's/\//\\\//g')/" ${FILE}
+    sed -i "s|^${PROP}.*|${PROP} = ${VALUE}|" ${FILE}
   else
-    echo -e "\n${PROP} = ${VALUE}" >> ${FILE}
+    echo "\n${PROP} = ${VALUE}" >> ${FILE}
   fi
 }
 
@@ -108,7 +99,7 @@ update_properties
 if [ "$1" = "start" ]; then
   shift
 
-  if [[ ! -f ${APP_DATA_BASE_DIR}/.first_start_completed ]]; then
+  if [ ! -f ${APP_DATA_BASE_DIR}/.first_start_completed ]; then
     wait_for_postgres
     init_postgres
     init_tomcat
