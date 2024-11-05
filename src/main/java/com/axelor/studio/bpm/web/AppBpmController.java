bf71ac2da9872ad8cd4fb4c46f9351256c9578ec
Abderrahim Aabane <a.aabane@axelor.com>
2024-11-05 16:56:57 +0100

    Created getCustomVariables action method

--- src/main/java/com/axelor/studio/bpm/web/AppBpmController.java
+++ src/main/java/com/axelor/studio/bpm/web/AppBpmController.java
@@ -1,14 +1,17 @@
 package com.axelor.studio.bpm.web;
 
+import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.script.GroovyScriptHelper;
 import com.axelor.studio.bpm.script.AxelorBindingsHelper;
+import com.axelor.studio.bpm.service.app.AppBpmService;
 import com.axelor.studio.db.AppBpm;
 import com.axelor.studio.db.CustomVariable;
 import com.axelor.studio.db.repo.CustomVariableRepository;
 import com.axelor.utils.helpers.ExceptionHelper;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.script.Bindings;
 import javax.script.SimpleBindings;
 
@@ -32,5 +35,14 @@ public class AppBpmController {
     } catch (Exception e) {
       ExceptionHelper.trace(response, e);
     }
+  }
+
+  public void getCustomVariables(ActionRequest request, ActionResponse response) {
+    response.setData(
+        Beans.get(AppBpmService.class).getAppBpm().getCustomVariableList().stream()
+            .filter(
+                customVariable ->
+                    customVariable.getStatus() == CustomVariableRepository.STATUS_VALID)
+            .collect(Collectors.toList()));
   }
 }
