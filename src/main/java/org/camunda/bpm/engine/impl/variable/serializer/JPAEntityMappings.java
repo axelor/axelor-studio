1e4d01eb8c70be39a8cca99d0820a80f2dff55a7
David Trochel <d.trochel@axelor.com>
2025-08-04 16:07:44 +0200

    refacto: avoid use of Character constructor

--- src/main/java/org/camunda/bpm/engine/impl/variable/serializer/JPAEntityMappings.java
+++ src/main/java/org/camunda/bpm/engine/impl/variable/serializer/JPAEntityMappings.java
@@ -154,7 +154,7 @@ public class JPAEntityMappings {
     } else if (type == Double.class || type == double.class) {
       return Double.parseDouble(string);
     } else if (type == Character.class || type == char.class) {
-      return new Character(string.charAt(0));
+      return string.charAt(0);
     } else if (type == Date.class) {
       return new Date(Long.parseLong(string));
     } else if (type == java.sql.Date.class) {
