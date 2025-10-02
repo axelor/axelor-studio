346d49a2a6e7d44bb621c65d952f49c708fe6046
Fatiha BEN BAICH <f.benbaich@axelor.com>
2025-10-02 12:46:23 +0200

    Merge branch 'studio-92906' into 'main'

--- src/main/java/com/axelor/studio/bpm/listener/WkfModelImportListener.java
+++ src/main/java/com/axelor/studio/bpm/listener/WkfModelImportListener.java
@@ -2,6 +2,7 @@ package com.axelor.studio.bpm.listener;
 
 import com.axelor.event.Observes;
 import com.axelor.events.StartupEvent;
+import com.axelor.inject.Beans;
 import com.axelor.studio.bpm.service.WkfBpmImportService;
 import com.axelor.utils.helpers.ExceptionHelper;
 import javax.annotation.Priority;
@@ -9,16 +10,10 @@ import javax.inject.Inject;
 
 public class WkfModelImportListener {
 
-  protected final WkfBpmImportService wkfBpmImportService;
-
-  @Inject
-  public WkfModelImportListener(WkfBpmImportService wkfBpmImportService) {
-    this.wkfBpmImportService = wkfBpmImportService;
-  }
 
   public void onStartUp(@Observes @Priority(value = -1) StartupEvent event) {
     try {
-      wkfBpmImportService.importDmn();
+      Beans.get(WkfBpmImportService.class).importDmn();
     } catch (Exception e) {
       ExceptionHelper.trace(e);
     }
