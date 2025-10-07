7bf60ffe7f3d10503abaa283fccd2cf47664337d
Fatiha BEN BAICH <f.benbaich@axelor.com>
2025-10-07 13:31:12 +0200

    Merge branch 'fix/var-management-issues' into 'main'

--- src/main/java/com/axelor/studio/bpm/listener/WkfModelImportListener.java
+++ src/main/java/com/axelor/studio/bpm/listener/WkfModelImportListener.java
@@ -6,10 +6,8 @@ import com.axelor.inject.Beans;
 import com.axelor.studio.bpm.service.WkfBpmImportService;
 import com.axelor.utils.helpers.ExceptionHelper;
 import javax.annotation.Priority;
-import javax.inject.Inject;
 
 public class WkfModelImportListener {
-
 
   public void onStartUp(@Observes @Priority(value = -1) StartupEvent event) {
     try {
